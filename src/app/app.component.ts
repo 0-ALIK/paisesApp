518d61baa18520544ce8d003687652b65d427e4f
Alik Dev <moisesalik01@gmail.com>
2022-12-06 19:59:33 -0500

    tailwind configurado y agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <p>{{title}}</p>`,
 })
 export class AppComponent {
   title = 'paisesApp';
