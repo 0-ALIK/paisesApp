ea0ddd5dbf2ba414aa82f861738f19672cbbdd3d
Alik Dev <moisesalik01@gmail.com>
2022-12-13 18:04:14 -0500

    avanzando con la siguiente parte del proyecto

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -5,11 +5,16 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-region',
   template: `
-    <app-pais-buscador
-      titleBuscador="Buscar por región"
-      descripBuscador="Realice una búsqueda por región"
-      (onEnter)="buscar( $event )">
-    </app-pais-buscador>
+    <h1 class="text-4xl font-bold">Por región</h1>
+    <h2 class="text-lg mt-2">Buscar país por región</h2>
+    <hr class="my-4">
+    <h2>Seleccione la región</h2>
+    <div class="flex gap-3">
+      <button
+        *ngFor="let region of regiones"
+        class="bg-sky-500 hover:bg-sky-400 py-1 px-2 rounded-md"
+        (click)="activarRegion( region )">{{region}}</button>
+    </div>
     <div *ngIf="hayError" class="bg-red-600 lg:mx-auto font-bold p-2 rounded-md my-4">
       No se encontro nada con el término "{{termino}}"
     </div>
@@ -20,6 +25,7 @@ export class PorRegionComponent {
   public termino: string = '';
   private _hayError: boolean = false;
   private _paises: IPaisAPI[] = [];
+  private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
   constructor (private paisService: PaisService) {}
 
@@ -31,19 +37,14 @@ export class PorRegionComponent {
     return this._hayError;
   }
 
-  public buscar(termino: string): void {
-    this._hayError = false;
-    this.termino = termino;
-
-    this.paisService.buscarPaisRegion(this.termino).subscribe(
-      (paises: IPaisAPI[]) => {
-        this._paises = paises;
-      },
-      (error) => {
-        this._hayError = true;
-        this._paises = [];
-        console.log(error);
-      }
-    );
+  public get regiones(): string[] {
+    return this._regiones;
   }
+
+  public activarRegion(region: string): void {
+    this.termino = region;
+  }
+
+
+
 }
